3d13df294d1aaa8a9440c2b3eef0972764e3ec9b
yulc <350009335@qq.com>
2020-02-07 22:52:01 +0800

    init

---
+++ demos/ribbon/README.md
@@ -0,0 +1,4 @@
+# RestTemplate + Ribbon 进行消费服务
+
+## RestTemplate
+一个访问第三方RESTful API接口的网络请求框架。
---
+++ demos/ribbon/src/test/java/org/ylc/frame/cloud/ribbon/RibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.ribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
