c05791b29476e77a3bb3f75a8483edbec96cdb82
superylcfly <350009335@qq.com>
2020-02-15 17:02:01 +0800

    init

---
+++ config/config-client/src/test/java/org/ylc/frame/cloud/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
