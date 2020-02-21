9e23049c1842c25478a7f4dbd0a69e89037b93b1
superylcfly <350009335@qq.com>
2020-02-21 16:56:35 +0800

    init

--- config/config-client/src/main/java/org/ylc/frame/cloud/configclient/ConfigClientApplication.java
+++ config/config-client/src/main/java/org/ylc/frame/cloud/configclient/ConfigClientApplication.java
@@ -2,8 +2,10 @@ package org.ylc.frame.cloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
