c0b948f4d309dc840c0a8fa63c8fd6452cac7630
yulc <350009335@qq.com>
2020-02-23 22:46:41 +0800

    init

---
+++ sleuth/src/main/java/org/ylc/frame/cloud/sleuth/SleuthApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthApplication.class, args);
+    }
+
+}
