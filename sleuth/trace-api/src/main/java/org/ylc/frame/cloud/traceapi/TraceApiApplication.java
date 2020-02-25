46c49ca67b01808da2529e67df47c508d767e142
superylcfly <350009335@qq.com>
2020-02-25 15:50:50 +0800

    Spring Cloud Sleuth & Zipkin with HTTP

---
+++ sleuth/trace-api/src/main/java/org/ylc/frame/cloud/traceapi/TraceApiApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.traceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceApiApplication.class, args);
+    }
+
+}
---
+++ sleuth/trace-api/src/test/java/org/ylc/frame/cloud/traceapi/TraceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.traceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
