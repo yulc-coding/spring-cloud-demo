a5fba6aae549507c885f007fa92d0290703b50c0
superylcfly <350009335@qq.com>
2020-02-28 16:29:37 +0800

    Nacos Discovery

---
+++ nacos/nacos-discovery/src/main/java/org/ylc/frame/cloud/nacosdiscovery/NacosDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.nacosdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryApplication.class, args);
+    }
+
+}
---
+++ nacos/nacos-discovery/src/main/java/org/ylc/frame/cloud/nacosdiscovery/controller/ApiController.java
@@ -0,0 +1,41 @@
+package org.ylc.frame.cloud.nacosdiscovery.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 代码千万行，注释第一行，
+ * 注释不规范，同事泪两行。
+ * <p>
+ * 对外接口
+ *
+ * @author YuLc
+ * @version 1.0.0
+ * @date 2020/2/28
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/request/{msg}")
+    public String request(@PathVariable String msg) {
+        System.out.println("get message：" + msg);
+        return "from port: " + port;
+    }
+
+    @GetMapping("/ribbon/{msg}")
+    public String forRibbon(@PathVariable String msg) {
+        System.out.printf("get message %s with ribbon\n", msg);
+        return "from port: " + port;
+    }
+
+    @GetMapping("/feign")
+    public String forFeign(@RequestParam(name = "msg") String msg) {
+        System.out.printf("get message %s with feign\n", msg);
+        return "from port: " + port;
+    }
+
+}
