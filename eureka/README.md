f5e040f9c0d67f9416f60e45a9945fc53a34a315
yulc <350009335@qq.com>
2020-03-02 21:00:37 +0800

    Eureka

--- eureka/README.md
+++ eureka/README.md
@@ -1,9 +1,54 @@
 ## Eureka服务注册
 
+### 介绍
+服务发现是微服务体系结构的核心内容之一。`Eureka`是`NetFlix`的一个子模块，用于服务的定位和注册，**只需要使用服务的标识，就可以访问服务**，它遵循分布式事务的**AP原则**。功能类似`Dubbo`的注册中心，比如`Zookeeper`
+
 ### Eureka Server
+主管服务注册，是服务注册中心，存储所有可用的服务节点信息。系统中的其他服务通过`Eureka`客户端注册到服务中心，并维持心跳连接，并且可以通过服务中心来发现其他服务进行调用。
+
+#### pom
+```
+    <dependency>
+        <groupId>org.springframework.cloud</groupId>
+        <artifactId>spring-cloud-starter-netflix-eureka-server</artifactId>
+    </dependency>
+```
+
+#### yml配置
+
+#### 引入Eureka服务注解：@EnableEurekaServer
+
+#### 
 
 ### Eureka Client
+每个客户端节点启动后会主动向`EurekaServce`注册自己，并维持一个心跳链接。当需要和其他服务通信时，会想`EurekaServce`询问目标服务的地址进行访问。
+
+#### pom
+```
+    <dependency>
+        <groupId>org.springframework.cloud</groupId>
+        <artifactId>spring-cloud-starter-netflix-eureka-client</artifactId>
+    </dependency>
+```
+
+#### yml配置
+
+#### 引入Eureka客户端注解：@EnableEurekaClient
+
 
 ### 自我保护机制(server)
+遵循：宁可保留错误的服务信息，也不会盲目注销任何可能健康的服务。
+* 它是一种应对网络异常的保护措施，生成环境建议开启。
+* 15分钟内超过85%的节点都没有正常的心跳，进入自我保护状态。
+* 自我保护状态下不会删除注册表中的服务信息。
+* 当它收到心跳数重新恢复到阈值以上时，退出自我保护状态。
 
+### Eureka和Zookeeper
+CAP:
+* C 强一致性
+* A 可用性
+* P 分区容错（分布式的核心）
+
+* Eureka AP原则
+* Zookeeper CP原则
 
--- eureka/eureka-client/src/main/java/org/ylc/frame/cloud/eurekaclient/controller/EurekaController.java
+++ eureka/eureka-client/src/main/java/org/ylc/frame/cloud/eurekaclient/controller/EurekaController.java
@@ -1,7 +1,11 @@
 package org.ylc.frame.cloud.eurekaclient.controller;
 
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 代码千万行，注释第一行，
@@ -14,8 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EurekaController {
 
+    private final DiscoveryClient discoveryClient;
+
+    public EurekaController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
     @GetMapping("/test")
-    public String test() {
-        return "success";
+    public Object test() {
+        List<String> services = discoveryClient.getServices();
+        System.out.println("services:" + services);
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("eureka-client");
+        for (ServiceInstance instance : instances) {
+            System.out.printf("instanceId: %s, serviceId: %s, host: %s, port: %s, uri: %s",
+                    instance.getInstanceId(),
+                    instance.getServiceId(),
+                    instance.getHost(),
+                    instance.getPort(),
+                    instance.getUri()
+            );
+        }
+        return this.discoveryClient;
     }
 }
