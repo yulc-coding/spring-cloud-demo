385dbb86a02a11def8a5f3318a309bd8c82c994d
yulc <350009335@qq.com>
2020-08-08 23:14:06 +0800

    upms

---
+++ framework/upms/src/main/java/org/ylc/frame/cloud/upms/UpmsApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpmsApplication.class, args);
+    }
+
+}
---
+++ framework/upms/src/test/java/org/ylc/frame/cloud/upms/UpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.upms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
