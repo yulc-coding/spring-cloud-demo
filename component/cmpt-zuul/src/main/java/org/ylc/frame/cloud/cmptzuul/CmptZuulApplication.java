c370b2f3ac4f2b412b7f4dd92a239b4c20081353
yulc <350009335@qq.com>
2020-08-13 22:30:10 +0800

    Zuul

---
+++ component/cmpt-zuul/src/main/java/org/ylc/frame/cloud/cmptzuul/CmptZuulApplication.java
@@ -0,0 +1,17 @@
+package org.ylc.frame.cloud.cmptzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class CmptZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmptZuulApplication.class, args);
+    }
+
+}
---
+++ component/cmpt-zuul/src/test/java/org/ylc/frame/cloud/cmptzuul/CmptZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.cmptzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmptZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
