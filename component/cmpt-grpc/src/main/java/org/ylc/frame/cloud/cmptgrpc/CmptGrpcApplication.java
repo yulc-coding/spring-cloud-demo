391091f04bb3fba4b7ca813d684f7f5a4a077374
yulc <350009335@qq.com>
2020-09-08 22:46:31 +0800

    gRPC

---
+++ component/cmpt-grpc/src/main/java/org/ylc/frame/cloud/cmptgrpc/CmptGrpcApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.cmptgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmptGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmptGrpcApplication.class, args);
+    }
+
+}
---
+++ component/cmpt-grpc/src/test/java/org/ylc/frame/cloud/cmptgrpc/CmptGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.cmptgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmptGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
