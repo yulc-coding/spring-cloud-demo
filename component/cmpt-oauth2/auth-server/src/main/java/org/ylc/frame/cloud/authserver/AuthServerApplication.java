ccf5002fe68e1e992c4465c55a1031519c87624a
yulc <350009335@qq.com>
2020-09-28 22:25:17 +0800

    oauth2->Mybatis

--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/AuthServerApplication.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/AuthServerApplication.java
@@ -1,8 +1,10 @@
 package org.ylc.frame.cloud.authserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("org.ylc.frame.cloud.authserver.mapper")
 @SpringBootApplication
 public class AuthServerApplication {
 
