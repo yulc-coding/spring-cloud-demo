9195515222812e110aafb119d23ab43f73e1807f
yulc <350009335@qq.com>
2020-10-05 22:35:03 +0800

    oauth2-server 数据库方式配置客户端信息

--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/controller/ClientInfoController.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/controller/ClientInfoController.java
@@ -1,7 +1,9 @@
 package org.ylc.frame.cloud.authserver.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,14 @@ import java.util.List;
  * 代码全万行，注释第一行
  * 注释不规范，同事泪两行
  * <p>
- * 客户端信息，Client-ID，client-secret等信息
+ * 当使用数据库存储客户端信息时的维护接口
+ * 客户端信息：Client-ID，client-secret等信息
  *
  * @author YuLc
  * @version 1.0.0
  * @date 2020-10-03
  */
+@ConditionalOnProperty(prefix = "oauth-store", name = "client", havingValue = "db")
 @RequestMapping("/auth/client")
 @RestController
 public class ClientInfoController {
@@ -34,13 +38,13 @@ public class ClientInfoController {
     }
 
     @PostMapping("/add")
-    public String addClient(@RequestBody ClientDetails clientDetails) {
+    public String addClient(@RequestBody BaseClientDetails clientDetails) {
         jdbcClientDetailsService.addClientDetails(clientDetails);
         return "success";
     }
 
     @PostMapping("/update")
-    public String updateClient(@RequestBody ClientDetails clientDetails) {
+    public String updateClient(@RequestBody BaseClientDetails clientDetails) {
         jdbcClientDetailsService.updateClientDetails(clientDetails);
         return "success";
     }
