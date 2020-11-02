59ae9563ea664a8775334a32a37724b267f0dcc3
Yulc <350009335@qq.com>
2020-11-02 22:07:58 +0800

    调整 OAuth Server 配置

--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/JwtTokenConfig.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/JwtTokenConfig.java
@@ -1,6 +1,7 @@
 package org.ylc.frame.cloud.authserver.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +24,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Slf4j
 public class JwtTokenConfig {
 
+    /**
+     * JWT 对称加密密钥
+     */
+    @Value("${spring.security.oauth2.jwt.signingKey}")
+    private String signingKey;
+
     @Bean
     public TokenStore tokenStore() {
         log.info("使用JWT生成Token");
@@ -32,8 +39,9 @@ public class JwtTokenConfig {
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        log.info("获取秘钥：{}", signingKey);
         // 用于签名
-        accessTokenConverter.setSigningKey("JWT-SIGNINT-KEY");
+        accessTokenConverter.setSigningKey(signingKey);
         return accessTokenConverter;
     }
 
--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/WebSecurityConfig.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package org.ylc.frame.cloud.authserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,6 +23,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService securityUserDetailsService;
 
     /**
      * 密码加密
@@ -43,9 +49,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(securityUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 
     public static void main(String[] args) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
