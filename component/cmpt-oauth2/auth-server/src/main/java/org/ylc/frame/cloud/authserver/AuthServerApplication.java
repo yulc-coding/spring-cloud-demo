c89f08e5f483a4fd502fb090818219b34386ee97
Yulc <350009335@qq.com>
2020-11-03 22:06:32 +0800

    Nacos 服务注册

--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/AuthServerApplication.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/AuthServerApplication.java
@@ -3,8 +3,10 @@ package org.ylc.frame.cloud.authserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("org.ylc.frame.cloud.authserver.mapper")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthServerApplication {
 
