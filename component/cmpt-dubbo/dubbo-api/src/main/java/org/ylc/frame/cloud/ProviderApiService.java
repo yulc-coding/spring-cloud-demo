5820e16b2b3f43b02e99e31b2aa72eb03f5a9c96
Yulc <350009335@qq.com>
2020-11-12 23:29:25 +0800

    Dubbo api

---
+++ component/cmpt-dubbo/dubbo-api/src/main/java/org/ylc/frame/cloud/ProviderApiService.java
@@ -0,0 +1,16 @@
+package org.ylc.frame.cloud;
+
+/**
+ * 代码全万行，注释第一行
+ * 注释不规范，同事泪两行
+ * <p>
+ * 服务提供者对外API接口
+ *
+ * @author YuLc
+ * @version 1.0.0
+ * @date 2020-11-12
+ */
+public interface ProviderApiService {
+
+    String getProvider();
+}
