a1c931a67d7daf3c19543413e7aa6f4d3b3e6ec2
Yulc <350009335@qq.com>
2020-11-13 21:12:08 +0800

    Dubbo provider

---
+++ component/cmpt-dubbo/dubbo-provider/src/test/java/org/ylc/frame/cloud/dubbo/DubboProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.cloud.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
