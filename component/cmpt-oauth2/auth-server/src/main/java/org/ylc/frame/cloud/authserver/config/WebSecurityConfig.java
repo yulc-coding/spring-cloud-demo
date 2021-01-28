2ddcbb4d2c57f009edd0d19b589a60f3db9f49a0
Yulc <350009335@qq.com>
2021-01-28 21:32:26 +0800

    OAuth2

--- component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/WebSecurityConfig.java
+++ component/cmpt-oauth2/auth-server/src/main/java/org/ylc/frame/cloud/authserver/config/WebSecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * 代码千万行，注释第一行，
  * 注释不规范，同事泪两行。
  * <p>
- * spring security 基础配置
+ * spring security 基础配置 Web安全配置类
  *
  * @author YuLc
  * @version 1.0.0
